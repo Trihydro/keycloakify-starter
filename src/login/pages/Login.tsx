80b9c5271b2e97a16ff1033bd816734e9d7e44b4
dmccoystephenson <dmccoystephenson@gmail.com>
2025-02-07 16:02:48 -0700

    Updated format of CONTACT_EMAILS to JSON to include more information

--- src/login/pages/Login.tsx
+++ src/login/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useState, type FormEventHandler } from "react";
+import { JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useState, type FormEventHandler } from "react";
 import { clsx } from "keycloakify/tools/clsx";
 import { useConstCallback } from "keycloakify/tools/useConstCallback";
 import type { PageProps } from "keycloakify/login/pages/PageProps";
@@ -209,13 +209,11 @@ export default function Login(props: PageProps<Extract<KcContext, { pageId: "log
                 <br />
                 <p>Can't log in? Please contact the following individuals for support.</p>
                 <ul>
-                    {
-                        contactEmails.split(",").map((email) => (
-                            <li>
-                                <a href={`mailto:${email}`}>{email}</a>
-                            </li>
-                        ))
-                    }
+                    {JSON.parse(contactEmails).map((contact: { email: string; name: string }) => (
+                        <li key={contact.email}>
+                            <strong>{contact.name}</strong> - <a href={`mailto:${contact.email}`}>{contact.email}</a>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </Template>
