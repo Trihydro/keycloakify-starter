735dd5cbce47f0ecd7a73a6a79abe88911978a9a
dmccoystephenson <dmccoystephenson@gmail.com>
2025-03-25 16:22:40 -0600

    Revert "Fix contact emails not being pulled in from the environment correctly"
    This reverts commit effbcf3a01974849d98f5af70f5b2c46bac655a4.

--- src/login/pages/Login.tsx
+++ src/login/pages/Login.tsx
@@ -16,7 +16,31 @@ if (result.wasPresent) {
     console.log("my_custom_param", result.value);
 }
 
-var contactEmails = process.env.CONTACT_EMAILS ?? "";
+var contact1email = "";
+var contact2email = "";
+
+const getEnvVar = (name: string): string => {
+    const value = process.env[name];
+    if (value === undefined) {
+        return "";
+    }
+    return value;
+}
+
+const setContactEmails = () => {
+    contact1email = getEnvVar("CONTACT1_EMAIL");
+    if (contact1email === "") {
+        console.error("CONTACT1_EMAIL is required");
+        return;
+    }
+    contact2email = getEnvVar("CONTACT2_EMAIL");
+    if (contact2email === "") {
+        console.error("CONTACT2_EMAIL is required");
+        return;
+    }
+}
+
+setContactEmails();
 
 export default function Login(props: PageProps<Extract<KcContext, { pageId: "login.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
@@ -207,15 +231,14 @@ export default function Login(props: PageProps<Extract<KcContext, { pageId: "log
             </div>
             <div>
                 <br />
-                <p>Can't log in? Please contact the following individuals for support.</p>
+                <p>Can't logaaaa in? Please contact the following individuals for support.</p>
                 <ul>
-                    {
-                        contactEmails.split(",").map((email) => (
-                            <li>
-                                <a href={`mailto:${email}`}>{email}</a>
-                            </li>
-                        ))
-                    }
+                    <li>
+                        <a href={`mailto:${contact1email}`}>{contact1email}</a>
+                    </li>
+                    <li>
+                        <a href={`mailto:${contact2email}`}>{contact2email}</a>
+                    </li>
                 </ul>
             </div>
         </Template>
